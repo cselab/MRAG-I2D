816d0f255a801258a5c10376e31a7d3761126ea6
Sergey Litvinov <slitvinov@gmial.com>
2026-06-08 14:04:09 +0200

    up levelset/onecase

--- levelset/onecase.cpp
+++ levelset/onecase.cpp
@@ -1,24 +1,41 @@
 #include <cmath>
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
 #include <string>
 
 #include "MRAGCommon.h"
 #include "MRAGEnvironment.h"
-#include "MRAGBlock.h"
 #include "MRAGrid.h"
 #include "MRAGRefiner.h"
 #include "MRAGCompressor.h"
-#include "MRAGBlockLab.h"
 #include "MRAGBlockFWT.h"
 #include "MRAGWavelets_AverageInterp5thOrder.h"
+#include "MRAGSimpleLevelsetBlock.h"
 #include "MRAGScienceCore.h"
 #include "MRAG_IO_XDMF.h"
 
 using namespace MRAG;
 
+struct LevelsetPoint {
+  Real phi;
+  LevelsetPoint(Real v = 0) : phi(v) {}
+  void operator+=(LevelsetPoint t) { phi += t.phi; }
+  operator Real() { return phi; }
+  Real levelset() const { return phi; }
+  Real giveMe(int, Real) const { return phi; }
+};
+LevelsetPoint operator*(const LevelsetPoint &p, Real v) {
+  return LevelsetPoint(p.phi * v);
+}
+template <typename T, int i> inline Real ls_projector_impl(const LevelsetPoint &t) {
+  return (Real)t.levelset();
+}
+make_projector(ls_projector, ls_projector_impl)
+
 typedef Wavelets_AverageInterp5thOrder W;
-typedef Block<Real, 8, 8, 1> B;
+typedef SimpleLevelsetBlock<LevelsetPoint, 4, 8, 8, 1> B;
+
 static const double R0 = 0.15;
 static double XC = 0.5;
 static double YC = 0.75;
@@ -35,24 +52,24 @@ static void _ic(Grid<W, B> &grid) {
       for (int ix = 0; ix < B::sizeX; ix++) {
         double x[2];
         info.pos(x, ix, iy);
-        block(ix, iy) = phi0(x[0], x[1]);
+        block(ix, iy) = LevelsetPoint(phi0(x[0], x[1]));
       }
   }
 }
 int main(int argc, char **argv) {
   const int nBlocks = 4;
-  const double tol = 1e-3;
+  const double tol = 1e-7;
   const int maxLevel = 5;
   const int nsteps = 20;
 
-  Environment::setup();
+  Environment::setup(argc > 1 ? atoi(argv[1]) : -1);
 
   Grid<W, B> grid(nBlocks, nBlocks, 1);
   Refiner refiner;
   Compressor compressor;
   grid.setRefiner(&refiner);
   grid.setCompressor(&compressor);
-  BlockFWT<W, B> blockfwt;
+  BlockFWT<W, B, ls_projector> blockfwt;
 
   std::vector<IO_XDMF<W, B>::Frame> frames;
   for (int step = 0; step < nsteps; step++) {
@@ -61,9 +78,10 @@ int main(int argc, char **argv) {
     YC = 0.5 + 0.2 * std::sin(2.0 * M_PI * t);
 
     _ic(grid);
-    Science::AutomaticRefinement<0, 0>(grid, blockfwt, tol, maxLevel, -1, _ic);
-    Science::AutomaticCompression<0, 0>(grid, blockfwt, tol);
-    printf("  step %d: grid has %zu blocks after refine+compress\n", step,
+    Science::AutomaticRefinementForLevelsets(grid, blockfwt, tol, maxLevel, true,
+                                             -1, NULL, _ic);
+    Science::AutomaticCompressionForLevelsets(grid, blockfwt, tol, true, 1);
+    printf("  step %d: grid has %zu blocks\n", step,
            grid.getBlocksInfo().size());
 
     char name[64];
